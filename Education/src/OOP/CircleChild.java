7861a9aea0b6888d291767e44c1109ae9bf672d5
Pernyei <pernyei.gabor@gmx.com>
2020-02-24 23:25:48 +0100

    GitHub Test

--- Education/src/OOP/CircleChild.java
+++ Education/src/OOP/CircleChild.java
@@ -2,13 +2,14 @@ package OOP;
 
 public class CircleChild extends Circle {
 
+    //SUPER: Az ős konstruktorának hívása, legalábbis itt
+    public CircleChild() {
+        super();
+    }
     public CircleChild(double r, double x, double y) {
         super(r,x,y);
     }
 
-    public CircleChild() {
-        super();
-    }
     public static void main(String[] args) {
         CircleChild circle0 = new CircleChild();
         CircleChild circle1 = new CircleChild(10.0, 10.0, 10.0);
