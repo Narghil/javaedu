a1ff3eed1e93c1bcda1e01ed0ba17af0e1122d54
Pernyei <pernyei.gabor@gmx.com>
2020-02-25 10:06:53 +0100

    GitHub Test

--- Education/src/Basics/ArrayPrint.java
+++ Education/src/Basics/ArrayPrint.java
@@ -1,5 +1,6 @@
 //Ezt a file-t használom a GitHub tesztelésére is.
 //- A GitHub-on lévő módosítás megjelent a lokális fileban.
+//- A lokális módosítások megjelentek a GitHub-on is, Commit ÉS Push után.
 package Basics;
 
 public class ArrayPrint {
