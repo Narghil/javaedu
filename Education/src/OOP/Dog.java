3b60bbe7bbf931d4c986cf2cc043c303599d287c
Narghil <pernyei.gabor@gmx.co>
2020-02-26 11:30:25 +0100

    //Órai munka

---
+++ Education/src/OOP/Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+    private String name, owner;
+    private int age;
+
+    @Override
+    public String toString(){
+        return String.format("The dog's name: %s, owners name: %s, age: %d", this.name, this.owner, this.age);
+    }
+
+    public Dog( String name, String owner, int age){
+        this.name = name; this.owner = owner; this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog( "Barker", "John Parker", 4);
+
+        System.out.println(dog.toString());
+    }
+}
