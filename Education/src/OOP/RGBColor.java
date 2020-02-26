32558733b44ef19cc9d108d275152a9f21c9077f
Narghil <pernyei.gabor@gmx.co>
2020-02-26 11:11:01 +0100

    //Órai munka

--- Education/src/OOP/RGBColor.java
+++ Education/src/OOP/RGBColor.java
@@ -3,37 +3,43 @@ package OOP;
 public class RGBColor {
     private int red, green, blue;
 
-    private boolean setRGB( int red, int green, int blue){
-        if( red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255){
+    private boolean validRGB( int i ){
+        return (i >= 0 && i <= 255);
+    }
+
+    private boolean setRGB(int red, int green, int blue) {
+        if (!(validRGB(red) && validRGB(green) && validRGB(blue))) {
             throw new IllegalArgumentException("Hibás paraméter");
         } else {
             this.red = red;
             this.green = green;
             this.blue = blue;
         }
-        return  true;
+        return true;
     }
 
-    private String toHex( int number){
+    private String toHex(int number) {
         String hex;
 
         hex = Integer.toHexString(number);
-        if( hex.length() < 2){ hex = "0".concat(hex);}
+        if (hex.length() < 2) {
+            hex = "0".concat(hex);
+        }
 
         return hex;
     }
 
-    public String toString( ) {
-        return "#"+toHex(this.red)+toHex(this.green)+toHex(this.blue);
+    public String toString() {
+        return "#" + toHex(this.red) + toHex(this.green) + toHex(this.blue);
     }
 
-    public RGBColor( int red, int green, int blue ){
-        setRGB( red, green, blue);
+    public RGBColor(int red, int green, int blue) {
+        setRGB(red, green, blue);
     }
 
     public static void main(String[] args) {
-        RGBColor color = new RGBColor( 1,12,123);
+        RGBColor color = new RGBColor(1, 12, 123);
 
-        System.out.println("Hexa color code:"+color.toString());
+        System.out.println("Hexa color code:" + color.toString());
     }
 }
