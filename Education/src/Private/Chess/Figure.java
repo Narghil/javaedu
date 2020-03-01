9a2d2f4e28fbef7a9a16d39eaeab5e509de4892f
Pernyei <pernyei.gabor@gmx.com>
2020-03-01 21:59:25 +0100

    GitHub Test

--- Education/src/Private/Chess/Figure.java
+++ Education/src/Private/Chess/Figure.java
@@ -4,13 +4,14 @@ import static Private.Chess.Constants.*;
 
 public abstract class Figure {
 
-    protected class Direction{
+    protected class Direction {
         int directionX;
         int directionY;
         Boolean repeating;
 
-        public Direction( int dx, int dy, boolean repeating ){
-            directionX = dx; directionY = dy;
+        public Direction(int dx, int dy, boolean repeating) {
+            directionX = dx;
+            directionY = dy;
             this.repeating = repeating;
         }
     }
@@ -41,7 +42,7 @@ public abstract class Figure {
         this.column = column;
     }
 
-    public void drawMe(){
+    public void drawMe() {
         System.out.print(this.representer);
     }
 
@@ -53,8 +54,13 @@ public abstract class Figure {
         setCoordinates(row, column);
     }
 
-    public int[][] getValidMovements(){return null;}
-    public boolean ifValidMovement( int row, int column){return false;}
+    public int[][] getValidMovements() {
+        return null;
+    }
+
+    public boolean ifValidMovement(int row, int column) {
+        return false;
+    }
 }
 
 class NoFigure extends Figure {
@@ -68,16 +74,23 @@ class NoFigure extends Figure {
 class Pawn extends Figure {
     public Pawn(Table ownerTable, int side, int row, int column) {
         super(ownerTable, PAWN, side, row, column);
-        if( side == WHITE){ representer = "P"; } else { representer = "p"; }
-        //A PAWN különleges módon mozog!
+        if (side == WHITE) {
+            representer = "P";
+        } else {
+            representer = "p";
+        }
         movementPattern = new Direction[4];
-        movementPattern[0] = new Direction(  0,1,false);
-        movementPattern[1] = new Direction(  0,2,false);
-        movementPattern[2] = new Direction( -1,1,false);
-        movementPattern[3] = new Direction(  1,1,false);
+        movementPattern[0] = new Direction(0, 1, false);
+        movementPattern[1] = new Direction(0, 2, false);
+        movementPattern[2] = new Direction(-1, 1, false);
+        movementPattern[3] = new Direction(1, 1, false);
     }
+
+    //A PAWN különleges módon mozog!
     @Override
-    public int[][] getValidMovements(){ return null; }
+    public int[][] getValidMovements() {
+        return null;
+    }
 
 }
 
@@ -85,36 +98,62 @@ class Knight extends Figure {
     public Knight(Table ownerTable, int side, int row, int column) {
         super(ownerTable, KNIGHT, side, row, column);
         //K for King. N for the (K)Night
-        if( side == WHITE){ representer = "N"; } else { representer = "n"; }
+        if (side == WHITE) {
+            representer = "N";
+        } else {
+            representer = "n";
+        }
     }
 }
 
 class Bishop extends Figure {
     public Bishop(Table ownerTable, int side, int row, int column) {
         super(ownerTable, BISHOP, side, row, column);
-        if( side == WHITE){ representer = "B"; } else { representer = "b"; }
+        if (side == WHITE) {
+            representer = "B";
+        } else {
+            representer = "b";
+        }
     }
 }
 
 class Rook extends Figure {
     public Rook(Table ownerTable, int side, int row, int column) {
         super(ownerTable, ROOK, side, row, column);
-        if( side == WHITE){ representer = "R"; } else { representer = "r"; }
+        if (side == WHITE) {
+            representer = "R";
+        } else {
+            representer = "r";
+        }
     }
 }
 
 class Queen extends Figure {
     public Queen(Table ownerTable, int side, int row, int column) {
         super(ownerTable, QUEEN, side, row, column);
-        if( side == WHITE){ representer = "Q"; } else { representer = "q"; }
+        if (side == WHITE) {
+            representer = "Q";
+        } else {
+            representer = "q";
+        }
     }
 }
 
 class King extends Figure {
     public King(Table ownerTable, int side, int row, int column) {
         super(ownerTable, KING, side, row, column);
-        if( side == WHITE){ representer = "K"; } else { representer = "k"; }
+        if (side == WHITE) {
+            representer = "K";
+        } else {
+            representer = "k";
+        }
     }
-    public boolean ifInCheck(){ return false;}
-    public boolean ifCheckMate(){return false;}
+
+    public boolean ifInCheck() {
+        return false;
+    }
+
+    public boolean ifCheckMate() {
+        return false;
+    }
 }
